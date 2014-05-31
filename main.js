adb34d19f1a00bd6bafd356bcc5225387f93b565
Anthony D'Andrea <anthony.dandrea@me.com>
2014-05-30 23:46:59 -0500

    something ugly happened when copying from notes

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
-  var input = "<input id='diffToggleInput' type='text' placeholder='Enter filter here' />"
-  var button = "<button id='diffToggleButton' style='margin-left:1em;'>Toggle Hidden Diffs</button>"
+	var input = "<input id='diffToggleInput' type='text' placeholder='Enter filter here' />"
+	var button = "<button id='diffToggleButton' style='margin-left:1em;'>Toggle Hidden Diffs</button>"
 
 	$('#diff').prepend(input + button);
 
 	var filterText = $('#diffToggleInput').val();
 
-  // Checks to make sure there is a val then hides and adds hidden-diff class
-  var filterInit = function(filterText) {
+	// Checks to make sure there is a val then hides and adds hidden-diff class
+	var filterInit = function(filterText) {
 		if (filterText != '') {
 			$('.file .js-selectable-text').each(function() {
-	      var fileName = $(this).text();
-	      if (fileName.indexOf(filterText) > -1) {
-	        $(this).closest('.file').addClass('hidden-diff').hide();
-	      };
-      });
+				var fileName = $(this).text();
+				if (fileName.indexOf(filterText) > -1) {
+					$(this).closest('.file').addClass('hidden-diff').hide();
+				};
+			});
 		};
 	};
-	
+
 	filterInit(filterText);
 
-  // Button toggle
+	// Button toggle
 	$('#files_bucket').on('click', '#diffToggleButton', function() {
-		$('.hidden-diff').toggle();
+		$('.hidden-diff').show().removeClass('hidden-diff');
+		filterText = $('#diffToggleInput').val();
+		filterInit(filterText);
 	});
 
 	// Input watch
@@ -31,5 +33,5 @@ $(document).ready(function() {
 		filterText = $('#diffToggleInput').val();
 		filterInit(filterText);
 	});
-	
+
 });
