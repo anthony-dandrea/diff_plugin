edf0e75e5f0b67ac49e88a06921060ba8c7bb271
Anthony D'Andrea <adandrea@leapfrogonline.com>
2014-12-17 14:48:05 -0600

    popup getting there

--- main.js
+++ main.js
@@ -1,53 +1,11 @@
 $(document).ready(function() {
-	var input = "<input id='diffToggleInput' type='text' placeholder='Enter filter here' />"
-	var button = "<button id='diffToggleButton' style='margin-left:1em;'>Toggle Hidden Diffs</button>"
 
-	$('#toc').prepend(input + button);
+	// http://stackoverflow.com/questions/13617257/saving-data-in-localstorage-for-a-googlechrome-extension-options-panel-and-acces
 
-	var filterText = $('#diffToggleInput').val();
-
-	// Checks to make sure there is a val then hides and adds hidden-diff class
-	var filterInit = function(filterText) {
-		if (filterText != '') {
-			$('.file .js-selectable-text').each(function() {
-				var fileName = $(this).text();
-				if (fileName.indexOf(filterText) > -1) {
-					$(this).closest('.file').addClass('hidden-diff').hide();
-				};
-			});
-		};
-	};
-
-	filterInit(filterText);
-
-	// Button toggle
-	$('#files_bucket').on('click', '#diffToggleButton', function() {
-		$('.hidden-diff').show().removeClass('hidden-diff');
-		filterText = $('#diffToggleInput').val();
-		filterInit(filterText);
+	var filterRegex;
+	chrome.storage.local.get('filter',function (obj){
+	    console.log(obj.filter);
+	    filterRegex = obj.filter;
 	});
 
-	// Input watch
-	$('#diffToggleInput').on('change', function(){
-		$('.hidden-diff').show().removeClass('hidden-diff');
-		filterText = $('#diffToggleInput').val();
-		filterInit(filterText);
-	});
-
-});
-
-
-var recetxt=""
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	recetxt=request.greeting
-});
-alert(recetxt)
-
-
-// http://stackoverflow.com/questions/13617257/saving-data-in-localstorage-for-a-googlechrome-extension-options-panel-and-acces
-
-var storesRegexPattern;
-chrome.storage.local.get('storesRegexPattern',function (obj){
-    console.log(obj.storesRegexPattern);
-    storesRegexPattern = obj.storesRegexPattern;
 });
--- popup.js
+++ popup.js
@@ -1,6 +1,27 @@
-var txt="TEXT"
-chrome.extension.getBackgroundPage().chrome.tabs.getSelected(null, function(tab) {
-  chrome.tabs.sendRequest(tab.id, {greeting: txt}, function(response) {
+function loadPreviousSetting() {
+	console.log('loadPreviousSetting running.');
 
-  });
-});
+	// Get current filter setting
+	chrome.storage.local.get('filter', function(obj){
+		if (obj) {
+			document.getElementById('filterInput').value = obj.filter;
+		}
+	});
+};
+
+function getFilterSetting() {
+	console.log('getFilterSetting running.');
+
+	// Get text input value for filter
+	var filter = document.getElementById('filterInput').value;
+	console.log('Filter:', filter);
+
+	// Set filter value
+	chrome.storage.local.set({'filter': filter});
+
+};
+
+window.onload = function (){
+	loadPreviousSetting();
+	document.getElementsByTagName('button').onclick = getFilterSetting;
+};
