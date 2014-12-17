5c9b696beef80bf9cdf0111a0b76f6d1bad45067
Anthony D'Andrea <adandrea@leapfrogonline.com>
2014-12-17 16:54:41 -0600

    trying to get popup to work still. this just plays w background script.

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+	function updateView() {
+		var fileContainers = $('.file');
+		fileContainer.each('')
+			var fileContainer = $(this);
+			var fileName = fileContainer.find('.js-selectable-text')
+	};
+
+	// For enterpreise
+	// $('.file .js-selectable-text');
+
 	// http://stackoverflow.com/questions/13617257/saving-data-in-localstorage-for-a-googlechrome-extension-options-panel-and-acces
 
 	var filterRegex;
